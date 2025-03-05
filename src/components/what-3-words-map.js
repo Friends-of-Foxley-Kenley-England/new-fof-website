f99ea021022bb16ca559ee3c2e763d6e5e732a97
eloisetaylor5693 <eloisetaylor5693@gmail.com>
2025-03-05 18:04:13 +0000

    example on website which doesn't load

--- src/components/what-3-words-map.js
+++ src/components/what-3-words-map.js
@@ -1,5 +1,8 @@
 import * as React from "react";
-import { What3wordsMap } from "@what3words/react-components";
+import {
+  What3wordsMap,
+  What3wordsAutosuggest,
+} from "@what3words/react-components";
 
 const API_KEY = process.env.GATSBY_W3W_API_KEY;
 const GOOLE_MAP_API_KEY = process.env.GATSBY_GOOGLE_MAPS_API_KEY;
@@ -19,7 +22,7 @@ const Map = () => {
     <section id="map-section">
       <h2>Map yar</h2>
       {console.log("What3wordsMap component:", What3wordsMap)}
-      <What3wordsMap
+      {/* <What3wordsMap
         id="w3w-map"
         api_key={API_KEY}
         map_api_key={GOOLE_MAP_API_KEY}
@@ -35,6 +38,31 @@ const Map = () => {
         // disable_default_ui={true}
         map_type_id="satellite">
         <div slot="map" id="map" />
+      </What3wordsMap> */}
+
+      <What3wordsMap
+        id="w3w-map"
+        api_key={API_KEY}
+        map_api_key={GOOLE_MAP_API_KEY}
+        zoom={18}
+        selected_zoom={20}
+        lng={-0.114637}
+        lat={51.454843}
+        search_control_position={2}
+        map_type_control={true}
+        zoom_control={true}
+        fullscreen_control={true}
+        fullscreen_control_position={3}
+        current_location_control_position={9}
+        disable_default_ui={true}
+        map_type_id="satellite"
+        words="filled.count.soap">
+        <div slot="map" id="map" />
+        <div slot="search-control">
+          <What3wordsAutosuggest>
+            <input type="text" placeholder="e.g. ///filled.count.soap" />
+          </What3wordsAutosuggest>
+        </div>
       </What3wordsMap>
     </section>
   );
