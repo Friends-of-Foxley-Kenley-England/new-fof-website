460380c9423a9278122c6f89abde8b72c11b26b6
Eloise Taylor <eloisetaylor5693@gmail.com>
2025-06-18 20:08:21 +0100

    Updated ancient beech tree copy from Anthony's updated copy

--- src/pages/trees/ancient-beech-tree-diversion.js
+++ src/pages/trees/ancient-beech-tree-diversion.js
@@ -1,7 +1,10 @@
 import Layout from "../../components/layout";
 import Seo from "../../components/seo";
+import { graphql } from "gatsby";
+import ExternalLink from "../../components/external-link";
 
-const AncientBeechTreeIndex = ({ location }) => {
+const AncientBeechTreeIndex = ({ data, location }) => {
+  const facebookLink = data?.site?.siteMetadata?.social?.facebook;
   return (
     <Layout location={location}>
       <h1>Ancient Beech Tree Diversion</h1>
@@ -25,6 +28,22 @@ const AncientBeechTreeIndex = ({ location }) => {
         rather than fell them.
       </p>
       <p>
+        The decay may have been caused by the pruning of a large limb, the
+        cavity shows fruiting bodies [brackets] of Ganoderma australe [Southern
+        Bracket] and has in the past also had both forms of Kretzschmaria deusta
+        [Brittle Cinder Fungus] That the latter has not been seen fruiting for
+        several years may be a hopeful sign that resources available to the
+        fungus may have been used up and the decay compartmentalised [reference:
+        CODIT]. Both of these fungi degrade the lignin in wood which provides
+        structural rigidity. The tree can sense the increase in flexing and can
+        respond by growing compensatory wood, of greater strength and density
+        than normal wood. This can be seen in the arch of new wood around the
+        cavity, which may actually be stronger than the original wood structure
+        of the trunk. However, it is not known how far any decay may have
+        extended up into the main branches, which could break out from the
+        pollard union.
+      </p>
+      <p>
         The tree was reduced in height by pruning several years ago to bring the
         crown below those of surrounding trees and reduce the potentially
         destructive effect of wind loading. Beech, especially over-mature
@@ -41,63 +60,60 @@ const AncientBeechTreeIndex = ({ location }) => {
       </p>
       <p>
         Rather than remove the risk of harm to users of the wood by felling the
-        tree, it’s extraordinary value as an ancient tree; the biodiversity of
+        tree, its' extraordinary value as an ancient tree; the biodiversity of
         the community of organisms to which it is a host, and an ecology in
         itself; its status as a local icon of the wood, and as a veteran tree
         registered on the national database; grant funding from the Mayor of
         London’s Greener City Fund was applied for and won to create the path
-        diversion and install fencing. The existing rolled stone path will also
-        be removed and the soil around the tree will be improved and decompacted
-        by adding a mixture of composted leaf mould and charcoal fines
-        [biochar]. This is intended to improve the condition of the rooting zone
-        of the tree with natural materials to assist its defence against the
-        decay fungi. The work will be carried out by a contractor, assisted by
-        the Friends and by Croydon TCV between the 5th and 16th of October. This
-        will entail closure of the path.
+        diversion and install fencing. In later work, the existing rolled stone
+        path will also be removed and the soil around the tree will be improved
+        and decompacted by adding a mixture of composted leaf mould and charcoal
+        fines [biochar]. This is intended to improve the condition of the
+        rooting zone of the tree with natural materials to assist its defence
+        against the decay fungi. The path diversion surfacing work will be
+        carried out by a contractor, assisted by the Friends and by Croydon TCV
+        between the 5th and 16th of October. This will entail closure of the
+        path from Woodland Way via the Picnic Glade to the Beech for the
+        duration of the work.
       </p>
       <p>
-        The decay may have been caused by the pruning of a large limb, the
-        cavity shows fruiting bodies [brackets] of Ganoderma australe [Southern
-        Bracket] and has in the past also had both forms of Kretzschmaria deusta
-        [Brittle Cinder Fungus]. That the latter has not been seen fruiting for
-        several years may be a hopeful sign that resources available to the
-        fungus may have been used up and the decay compartmentalised [reference:
-        CODIT]. Both of these fungi degrade the lignin in wood which provides
-        structural rigidity. The tree can sense the increase in flexing and can
-        respond by growing compensatory wood, of greater strength and density
-        than normal wood. This can be seen in the arch of new wood around the
-        cavity, which may actually be stronger than the original wood structure
-        of the trunk. However, it is not known how far any decay may have
-        extended up into the main branches, which could break out from the
-        pollard union. In clearing the proposed route for the diversion, three
-        badger holes were found adjacent to, and one directly in the route.
-        Monitoring of these holes over several months, including with trail
-        cameras, showed that only one is active and this is concealed behind a
-        fallen tree. Advice was obtained from the East Surrey Badger Protection
-        Society which has inspected the site and assisted in obtaining the
-        necessary authorisation and licence from Natural England for work to
-        take place adjacent to a sett.
+        In clearing the proposed route for the diversion, three badger holes
+        were found adjacent to, and one directly in the route. Monitoring of
+        these holes over several months, including with trail cameras, showed
+        that only one is active and this is concealed behind a fallen tree.
+        Advice was obtained from the East Surrey Badger Protection Society which
+        has inspected the site and assisted in obtaining the necessary
+        authorisation and licence from Natural England for work to take place
+        adjacent to a sett.
       </p>
       <p>
         It is understood and expected that restricting access to such a
         well-loved tree will be distressing to many users, some of whom have
-        described it to the Friends as ‘’their’’ tree. So many people have
-        engaged closely with the bark, the trunk, climbed it and swung from the
-        branch swings, admired and revelled in it’s glory, brought children to
-        experience its extraordinary character. But we need to protect the tree,
-        and the users, who will at least still be able to see it, even if at
-        something of a distance, via vistas we will create through the
+        described it to the Friends as "their" tree. So many people have engaged
+        closely with the bark, the trunk, climbed it and swung from the branch
+        swings, admired and revelled in its' glory, brought children to
+        experience its extraordinary character. But it is necessary to protect
+        the tree, and the users, who will at least still be able to see it, even
+        if at something of a distance, via vistas we will create through the
         surrounding trees, and from re-sited benches, hopefully for many more
         years to come. The only alternative at present is to fell it, and no-one
-        wants that….
+        wants that...
       </p>
       <p>
         If you have any concerns or questions, please contact the Friends of
-        Foxley via this site or our Facebook page [Friends of Foxley Wood].
+        Foxley via{" "}
+        <a href="mailto:foxleygcproject@gmail.com" rel="noopener noreferrer">
+          foxleygcproject@gmail.com
+        </a>
+        , this website <a href="/">www.friendsoffoxley.co.uk</a> , or our{" "}
+        <ExternalLink href={facebookLink}>
+          Facebook page [Friends of Foxley Wood]
+        </ExternalLink>
+        .
       </p>
       <p>
         Written by <strong>Anthony Mills, Dip.Hort[RHS]</strong>; Tech Arbor.A..
-        Arboricultural Advisor for Friends of Foxley 19th Oct 2020
+        Arboricultural Advisor for Friends of Foxley 26th Oct 2020
       </p>
     </Layout>
   );
@@ -114,3 +130,15 @@ export function Head({ location }) {
     />
   );
 }
+
+export const pageQuery = graphql`
+  query FacebookLink {
+    site {
+      siteMetadata {
+        social {
+          facebook
+        }
+      }
+    }
+  }
+`;
