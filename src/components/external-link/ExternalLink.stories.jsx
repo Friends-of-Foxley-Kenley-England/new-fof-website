9072147adf6846ff6d1a695b7da388dbf4d875d3
Eloise Taylor <eloisetaylor5693@gmail.com>
2025-10-12 11:27:26 +0100

    Add stories #330

---
+++ src/components/external-link/ExternalLink.stories.jsx
@@ -0,0 +1,31 @@
+import ExternalLink from './external-link';
+
+export default {
+  title: 'Components/ExternalLink',
+  component: ExternalLink,
+  parameters: {
+    layout: 'padded',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    href: {
+      control: 'text',
+      description: 'URL to link to',
+    },
+    'aria-label': {
+      control: 'text',
+      description: 'Accessible label for the link',
+    },
+    children: {
+      control: 'text',
+      description: 'Link content',
+    },
+  },
+};
+
+export const Default = {
+  args: {
+    href: 'https://example.com',
+    children: 'Visit Example',
+  },
+};
