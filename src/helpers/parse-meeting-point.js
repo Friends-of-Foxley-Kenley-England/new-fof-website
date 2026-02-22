54b8cc63e010f74dea366abe18a613de08d5400e
Eloise Taylor <eloisetaylor5693@gmail.com>
2026-02-22 18:57:43 +0000

    Map lat and long for each w3w location

--- src/helpers/parse-meeting-point.js
+++ src/helpers/parse-meeting-point.js
@@ -1,13 +1,19 @@
 const locationMappings = [
   {
+    latitude: 51.326099,
+    longitude: -0.110349,
     meetingPointWhatThreeWords: "tune.then.global",
     meetingPointDescription: "At the entrance of Burwood Avenue",
   },
   {
+    latitude: 51.330356,
+    longitude: -0.116181,
     meetingPointWhatThreeWords: "loves.final.needed",
     meetingPointDescription: "At the entrance in Woodland Way",
   },
   {
+    latitude: 51.326684,
+    longitude: -0.112252,
     meetingPointWhatThreeWords: "drives.slime.gates",
     meetingPointDescription:
       "At the picnic tables located along the top path where Sherwood Oaks Field starts",
