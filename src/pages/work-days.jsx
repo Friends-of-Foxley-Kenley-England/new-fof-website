94ddd17ae1ab62ad242e87e1e545c248d2d88c94
Eloise Taylor <eloisetaylor5693@gmail.com>
2026-03-12 18:11:33 +0000

    Update meeting time on the volunteer page

--- src/pages/work-days.jsx
+++ src/pages/work-days.jsx
@@ -28,8 +28,8 @@ const WorkDaysIndex = ({ data, location }) => {
         and footwear and bring your own refreshments.
       </p>
       <p>
-        We usually meet at 11am on the{" "}
-        <strong>second Sunday of the month</strong> and 10am on the{" "}
+        We usually meet at 10am on the{" "}
+        <strong>second Sunday of the month</strong> and on the{" "}
         <strong>last Wednesday of the month</strong>.
       </p>
       <p>
